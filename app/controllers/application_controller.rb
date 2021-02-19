8e0626d7d29ef8119d96a4ace03b002374cb1bfc
cruzgerman216 <cruzgerman216@yahoo.com>
2021-02-19 15:04:12 -0600

    protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
+    protect_from_forgery unless: -> { request.format.json? }
     helper_method :logged_in, :current_user
 
     def logged_in? 
