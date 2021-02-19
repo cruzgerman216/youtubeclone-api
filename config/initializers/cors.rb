a89738e487a6aae3afc5a58a753600b01d118728
cruzgerman216 <cruzgerman216@yahoo.com>
2021-02-19 14:05:01 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://germancruzclone.web.app/'
+    origins 'http://localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
   end
+    allow do
+      origins 'https://germancruzclone.web.app/'
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
+    end
   end
